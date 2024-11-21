1cc83589ff1c93118fdd80a3af3a828eeb5b05f7
rafli23l <amelmetha12@gmail.com>
2024-11-21 11:08:09 +0700

    MEMBUAT PROGRAM DHT11 OUTPUT SERIAL MONITOR
    dengan melakukan percobaan ini kita mengetahui berapa nilai kelembaban suhu di suatu daerah

---
+++ LKPD_12_RAFLI.ino.ino
@@ -0,0 +1,21 @@
+//RAFLI MAULANA
+//XI TAV 1
+#include"DHT.h"
+#define DHTPIN 32
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(9600);
+  dht.begin();}
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Gagal membaca dari sensor DHT11");
+  } else {
+    Serial.print("kelembaban: ");
+    Serial.print(humidity);
+    Serial.print("% SUHU: ");
+    Serial.print(temperature);
+    Serial.println("C");}
+delay(2000);}
